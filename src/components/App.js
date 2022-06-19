cf1a7273ee0c40f41d1e1383f48929c9c84a6c39
KatEscaner <jjmateo1279@gmail.com>
2022-06-19 22:52:56 +0200

    Update App.js
    I changed strictmode from reactDOM

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import React from 'react';
+import { v4 as uuidv4 } from 'uuid'
+import React, { useEffect, useState } from 'react';
 import Header from './Header';
 import AddContact from './AddContact';
 import ContactList from './ContactList';
@@ -7,11 +8,36 @@ import ContactList from './ContactList';
 
 
 function App() {
+  const LOCAL_STORAGE_KEY = 'contacts';
+  const [contacts, setContacts] = useState([]);
+
+  const addContactHandler = (contact) => {
+    console.log(contact);
+    setContacts([...contacts, { id: uuidv4(), ...contact}]);
+  }
+
+  const removeContactHandler = (id) => {
+    const newCotactList = contacts.filter((contact) => {
+      return contact.id !== id;
+    });
+    setContacts(newCotactList);
+  }
+
+  useEffect(() => {
+    const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if (retriveContacts) setContacts(retriveContacts);
+    console.log(retriveContacts);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
+
   return (
-    <div>
+    <div className='ui container'>
       <Header />
-      {/* <AddContact />
-      <ContactList /> */}
+      <AddContact addContactHandler={addContactHandler}/>
+      <ContactList contacts={contacts} getContactId={removeContactHandler}/>
     </div>
   );
 }
